12a1d42659b9e8c30c3f2e1ea036246092de6e22
Ritvik5 <sharma.ritvik56@gmail.com>
2023-06-15 02:05:17 +0530

    [Ritvik]Add: Welcome Message

---
+++ GenericsMaxMin/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GenericsMaxMin
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Generics!");
+        }
+    }
+}
