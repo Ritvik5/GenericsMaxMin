ea8f8a30c3c44dfa154fc9b2da4bc10faea71e53
Ritvik5 <sharma.ritvik56@gmail.com>
2023-06-15 02:18:33 +0530

    [Ritvik]Add: Finding Max float using CompareTo method.

--- GenericsMaxMin/Program.cs
+++ GenericsMaxMin/Program.cs
@@ -6,8 +6,15 @@
         {
             Console.WriteLine("Welcome to Generics!\n");
 
+            // -- Maximum Integers using CompareTo
+
             int result = FindMaximum.FindMax(23, 25, 27);
-            Console.WriteLine("Max Integer is:" + result);
+            Console.WriteLine("Max Integer is: " + result);
+
+            // -- Maximum Float using CompareTo
+
+            float result1 = FindMaximum.FindMaxFloat(22.4f, 23.4f, 21.5F);
+            Console.WriteLine("Max Float is: " + result1);
         }
     }
 }
