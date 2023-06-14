777793c63fb7705fda16015421588c2366abcc7e
Ritvik5 <sharma.ritvik56@gmail.com>
2023-06-15 02:12:38 +0530

    [Ritvik]Add: Finding Maximum Integer using CompareTo method.

--- GenericsMaxMin/Program.cs
+++ GenericsMaxMin/Program.cs
@@ -4,7 +4,10 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Generics!");
+            Console.WriteLine("Welcome to Generics!\n");
+
+            int result = FindMaximum.FindMax(23, 25, 27);
+            Console.WriteLine("Max Integer is:" + result);
         }
     }
 }
